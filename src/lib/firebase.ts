2f6888b4a2d27356666852dc205dbd6eb29a7e88
ken5saito <ken550914@gmail.com>
2026-02-19 22:47:23 +0900

    ビルド時のfirebaseエラー調整

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
@@ -11,5 +11,11 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app);
+function getFirebaseApp() {
+  if (!getApps().length) {
+    return initializeApp(firebaseConfig);
+  }
+  return getApp();
+}
+
+export const database = getDatabase(getFirebaseApp());
